5bde1857279df61bd8a26945e0d070a3dedfe9b4
Alexey Pryadka <todoaletodo@gmail.com>
2021-06-02 11:36:06 +0300

    Laba 3

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button_clicked(object sender, EventArgs e) 
+        {
+            Label1.Text = Entry1.Text;
+
+        }
+    }
+}
